5e8fb1853b9076a936c52a7d24c5d76340a8f875
Ravikanth Garimella <ravikanthdba.1987@gmail.com>
2019-04-12 20:05:06 +0530

    Hello World

---
+++ 001-hello-world/helloworld.go
@@ -0,0 +1,35 @@
+// Entry point to the program
+// Exit to the program is when it ecits the main function
+
+/*
+
+In the below program, the program starts with main function and goes in sequential order.
+First it prints "GoLanguage is Fun!!"
+next - it gors to the foo function
+It then prints "I am in 'foo' function"
+Next it prints "Printing in next line"
+Then it hits the closed parenthesis of the main function. This is when the code would exit.
+Hence, the code would end here.
+*/
+
+package main // Compulsory
+
+import "fmt"
+
+func main() {
+	fmt.Println("GoLanguage is Fun!!")
+	foo()
+	fmt.Println("Printing in next line")
+
+	for i := 0; i <= 100; i++ {
+		if i%2 == 0 {
+			fmt.Println("Even Number : ", i)
+		} else {
+			fmt.Println("Odd Number  : ", i)
+		}
+	}
+}
+
+func foo() {
+	fmt.Println("I am in 'Foo' function")
+}
