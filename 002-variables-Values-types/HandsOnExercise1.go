8457c8481f565785708b87e6b6158f82146d5d68
Ravikanth Garimella <ravikanthdba.1987@gmail.com>
2019-04-13 02:34:16 +0530

    Hands on Exercises: Variables, Values and Types

---
+++ 002-variables-Values-types/HandsOnExercise1.go
@@ -0,0 +1,25 @@
+/*
+    Hands On Exercise1 - USing short declaration operator, assign values to variables x, y, and z
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	x := 42;
+	y := "James Bond";
+	z := true;
+
+    // Print variables using single print statement
+    // Print variables using multiple print statement
+
+    fmt.Println("Single Line Statement");
+    fmt.Println("---------------------");
+    fmt.Printf("The value of x is : %d, y is %s and z is %t\n\n", x, y, z);
+    fmt.Println("Multi Line Statement");
+    fmt.Println("---------------------");
+    fmt.Println(x);
+    fmt.Println(y);
+    fmt.Println(z);
+}
---
+++ 002-variables-Values-types/HandsOnExercise4.go
@@ -0,0 +1,31 @@
+/*
+Hands-on exercise #4
+FYI - nice documentation and new terminology “underlying type”
+https://golang.org/ref/spec#Types 
+For this exercise
+Create your own type. Have the underlying type be an int.
+create a VARIABLE of your new TYPE with the IDENTIFIER “x” using the “VAR” keyword
+in func main
+print out the value of the variable “x”
+print out the type of the variable “x”
+assign 42 to the VARIABLE “x” using the “=” OPERATOR
+print out the value of the variable “x”
+*/
+
+
+package main
+
+import "fmt"
+
+type myVariableType int;
+var x myVariableType;
+
+func main() {
+    fmt.Printf("The type of the variable is %T\n", x);
+    fmt.Println(x);
+
+    x = 42;
+    fmt.Printf("The type of the variable is %T\n", x);
+    fmt.Println(x);
+}
+
